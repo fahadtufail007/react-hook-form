d221239ca1a8f7e733774fc85b613f437734a5ad
Asad <asad.thedevcorporate@gmail.com>
2022-12-14 11:58:20 +0500

    Small change

---
+++ src/Screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,21 @@
+// import { Box } from "@mui/system";
+import { Button, Input } from "@mui/material"; 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import React from "react";
+import './style.css';
+
+const HomeScreen = () => {
+  return(
+    <div>
+      <Button>text</Button>
+      <div>
+        <h4>hello</h4>
+      <TextField variant="outlined"/>
+      </div>
+      <Input/>
+    </div>
+  )
+}
+
+export default HomeScreen;
